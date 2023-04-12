5e2e2338f7a89a27e51e48497fce1fcbc811169d
ivan1395 <ivanexplosion1995@gmail.com>
2023-04-12 13:58:28 +0200

    SNAPSHOT 1

---
+++ NumerosAleatorios.psc
@@ -0,0 +1,14 @@
+Algoritmo sin_titulo
+	Definir suma,n1, n2 Como Entero
+	n1 = Aleatorio(45, 100)
+	Escribir n1
+	n2 = Aleatorio(45, 100)
+	Escribir n2
+	suma = n1 + n2
+	Escribir suma
+	Si suma % 2 = 0 Entonces
+		Escribir "La suma es par"
+	SiNo
+		Escribir "La suma es impar"
+	Fin Si
+FinAlgoritmo
---
+++ inicio.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+clear
+# git init
+git config user.name "ivan1395"
+git config user.email "ivanexplosion1995@gmail.com"
+git config user.password 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
+git add .
+git commit -m "SNAPSHOT 1"
+git remote add origin "https://github.com/ivan1395/Ejercicios-clase-12-04-2023.git"
+git branch -M main
+git push -u origin main
