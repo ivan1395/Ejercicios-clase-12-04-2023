f7ea5a788b5bd3a4a5f669d37c397ddb7482ac3e
ivan1395 <ivanexplosion1995@gmail.com>
2023-04-12 14:26:26 +0200

    SNAPSHOT 2

---
+++ UsuarioYContraseña.psc
@@ -0,0 +1,15 @@
+Algoritmo sin_titulo
+	definir usuario, contraseña Como Caracter
+	
+	Mientras Verdadero Hacer
+		escribir ("Usuario: ")
+		leer usuario
+		Escribir ("contraseña: ")
+		leer contraseña
+		si usuario == ("walter") y contraseña == ("1234")Entonces
+			escribir("¡Acceso concedido!")
+		SiNo
+			escribir("Código de usuario o contraseña incorrectos. Inténtalo de nuevo.")
+		Fin Si
+	Fin Mientras
+FinAlgoritmo
--- inicio.sh
+++ inicio.sh
@@ -5,7 +5,7 @@ git config user.name "ivan1395"
 git config user.email "ivanexplosion1995@gmail.com"
 git config user.password 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
 git add .
-git commit -m "SNAPSHOT 1"
-git remote add origin "https://github.com/ivan1395/Ejercicios-clase-12-04-2023.git"
+git commit -m "SNAPSHOT 2"
+git remote add origin1 "https://github.com/ivan1395/Ejercicios-clase-12-04-2023.git"
 git branch -M main
-git push -u origin main
+git push -u origin1 main
