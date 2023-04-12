ee6744078b8e68934c72886c62c1ef7f562014d7
ivan1395 <ivanexplosion1995@gmail.com>
2023-04-12 14:30:36 +0200

    User,Paswwor

--- inicio.sh
+++ inicio.sh
@@ -4,8 +4,8 @@ clear
 git config user.name "ivan1395"
 git config user.email "ivanexplosion1995@gmail.com"
 git config user.password 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
-git add .
-git commit -m "SNAPSHOT 2"
-git remote add origin1 "https://github.com/ivan1395/Ejercicios-clase-12-04-2023.git"
+#git add .
+#git commit -m "SNAPSHOT 2"
+git remote add origin2 "https://github.com/ivan1395/Ejercicios-clase-12-04-2023.git"
 git branch -M main
-git push -u origin1 main
+git push -u origin2 main
